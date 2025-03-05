e130a6f0ecf1cbb7266dd469261c1d3c54cbd2aa
Ali-Ehtisham <156206027+haid3r-ish@users.noreply.github.com>
2025-03-05 21:28:46 +0500

    Update Plisten.c

--- Plisten.c
+++ Plisten.c
@@ -3,10 +3,9 @@
 void pinListen(void *pvParameters) {
   uint8_t pState1 = 0, pState2 = 0, pState3 = 0, count = 0, current = 0,pCount = 0; 
   while (true) {
-    // Convert to digital according to threashold and Combine the two states using bitwise operations
     uint8_t current = (((analogRead(4) > 2000) ? 1 : 0) << 1) | ((analogRead(5) > 2000) ? 1 : 0);
-    if (current != 3) {  // Skip if "11"
-      if (pState1 != current) {  // Only proceed if "current" is different from the previous state
+    if (current != 3) { 
+      if (pState1 != current) {  // 
         if (current == 0) { // When both sensors are "00"
           if (pState2 != 0) {
             // Here see if count is 1 then set 1 or if zero then set 0 
